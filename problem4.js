f1338232c639f25f2b463f519011e96791dac300
unknown <4friizal@gmail.com>
2022-06-27 19:06:19 +0700

    Tugas Problem 1,2,3,4 dan 5

---
+++ problem4.js
@@ -0,0 +1,6 @@
+let alas = 20;
+let tinggi = 30;
+
+result=alas*tinggi/2
+
+console.log(result)
---
+++ problem5.js
@@ -0,0 +1,14 @@
+let T = 20;
+let r = 4;
+let sum = T + r
+
+// jika jari-jari nyaaa bisa di bagi 7
+if ( r % 7 == 0 ) {
+    result = 2*22/7*r*sum
+}
+// jika jari-jari tidak bisa di bagi 7
+else {
+    result = 2*3.14*r*sum
+}
+
+console.log(result)
